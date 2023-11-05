807b3225a44e2395c5315ce5dc885d7bb515c8bd
Gabisanth <s.gabisanth11@gmail.com>
2023-11-05 20:28:03 +0000

    Removed test file for time being

--- test/runtests.jl
+++
@@ -1,8 +0,0 @@
-using MaximumAreaCoverageOptimization
-using Test
-
-# @testset "MaximumAreaCoverageOptimization.jl" begin
-#     @test MaximumAreaCoverageOptimization.greet_my_package() != "Hello world!"
-#     @test MaximumAreaCoverageOptimization.greet_my_package() == "Hello!"
-# end
-
