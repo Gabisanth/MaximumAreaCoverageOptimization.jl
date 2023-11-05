d819e4e003929139afe8fb8ad181ad397260a8be
Gabisanth <s.gabisanth11@gmail.com>
2023-11-05 21:00:40 +0000

    Revert "Removed test file for time being"
    This reverts commit 807b3225a44e2395c5315ce5dc885d7bb515c8bd.

---
+++ test/runtests.jl
@@ -0,0 +1,8 @@
+using MaximumAreaCoverageOptimization
+using Test
+
+# @testset "MaximumAreaCoverageOptimization.jl" begin
+#     @test MaximumAreaCoverageOptimization.greet_my_package() != "Hello world!"
+#     @test MaximumAreaCoverageOptimization.greet_my_package() == "Hello!"
+# end
+
