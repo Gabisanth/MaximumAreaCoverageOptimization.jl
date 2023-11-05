f00971a4180191909a463b82c783326ff16eaaee
Gabisanth <s.gabisanth11@gmail.com>
2023-11-05 20:59:55 +0000

    Revert "Updates to fix julia package errors"
    This reverts commit a12dff6a2a7df9c50a29ada676037110b0984da2.

--- test/runtests.jl
+++
@@ -1,6 +0,0 @@
-using MaximumAreaCoverageOptimization
-using Test
-
-@testset "MaximumAreaCoverageOptimization.jl" begin
-    @test MaximumAreaCoverageOptimization.greet_my_package() != "Hello world!"
-end
