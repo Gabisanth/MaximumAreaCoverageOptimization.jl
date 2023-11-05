a12dff6a2a7df9c50a29ada676037110b0984da2
Gabisanth <s.gabisanth11@gmail.com>
2023-11-05 20:49:34 +0000

    Updates to fix julia package errors

---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using MaximumAreaCoverageOptimization
+using Test
+
+@testset "MaximumAreaCoverageOptimization.jl" begin
+    @test MaximumAreaCoverageOptimization.greet_my_package() != "Hello world!"
+end
